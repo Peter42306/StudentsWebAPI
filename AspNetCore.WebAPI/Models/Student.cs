5d1a1ebde4e8ef68401b6d9ac22ada8df2090df6
Petr Zalizko <pzalizko@gmail.com>
2025-06-11 10:14:22 +0300

    Minor changes

--- AspNetCore.WebAPI/Models/Student.cs
+++ AspNetCore.WebAPI/Models/Student.cs
@@ -30,21 +30,12 @@ namespace AspNetCore.WebAPI.Models
 
     }
 
-
-
-
-
     public class Student
-    {
-        // Идентификатор студента
-        public int Id { get; set; }
-        // Имя студента
-        public string? Name { get; set; }
-        // Фамилия студента
-        public string? Surname { get; set; }
-        // Возраст студента
-        public int Age { get; set; }
-        // Средний балл
+    {        
+        public int Id { get; set; }        
+        public string? Name { get; set; }        
+        public string? Surname { get; set; }        
+        public int Age { get; set; }        
         public double GPA { get; set; }
     }
 }
